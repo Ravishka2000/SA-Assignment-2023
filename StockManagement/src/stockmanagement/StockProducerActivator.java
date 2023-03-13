e516aad58d2b305a7b0bd46c48452d0a0b2bee6c
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-03-13 21:24:54 +0530

    first commit

---
+++ StockManagement/src/stockmanagement/StockProducerActivator.java
@@ -0,0 +1,23 @@
+package stockmanagement;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class StockProducerActivator implements BundleActivator {
+
+	ServiceRegistration<?> serviceRegistration;
+	
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Inventory Service Started......");
+		StockService inventory = new StockServiceImpl();
+		
+		serviceRegistration = context.registerService(StockService.class.getName(), inventory, null);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Inventory Service Stopped.....");
+		serviceRegistration.unregister();
+	}
+
+}
